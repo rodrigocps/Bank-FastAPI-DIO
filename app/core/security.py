d86e0484bcd811bfbf2cf55226d51921c9ae3053
Rodrigo <roo_cps@hotmail.com>
2026-07-14 21:24:07 -0300

    feat: fluxo de login seguro e proteção de rotas

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,15 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from datetime import datetime, timedelta, timezone
 import jwt
+from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
+
+from app.core.database import get_db
 from app.core.config import settings
+from app.models.account import User
 
 # Configura o contexto do hash de senha com bcrypt
 pwd_context = CryptContext(schemas=["bcrypt"], deprecated="auto")
@@ -23,3 +31,33 @@ def create_access_token(data: dict) -> str:
 # Codifica o token utilizando a chave e algoritmo do .env
     encode_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encode_jwt
+
+# Esquema OAuth2 que extrai o token do cabeçalho "Authorization: Beaer <token>"
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+async def get_current_user(
+    token: str = Depends(oauth2_schema),
+    db: AsyncSession = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Unable to validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        # Decodifica o token usando a chave secreta
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except InvalidTokenError:
+        raise credentials_exception
+    
+    # Busca o usuario no banco de dados
+    result = await db.execute(select(User).where(User.username == username))
+    user = result.scalars().first()
+    
+    if user is None:
+        raise credentials_exception
+    return user
